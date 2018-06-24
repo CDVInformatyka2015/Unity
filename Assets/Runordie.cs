05a16e94f2a1e1068b96ed52e377cd86edae4091
Otlet <otlet@otlet.pl>
2018-06-24 11:16:51 +0200

    Test

--- Assets/Runordie.cs
+++ Assets/Runordie.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 public class Runordie : MonoBehaviour {
 
     public Button clicker;
-    public Animator left;
-    public Animator right;
+    public Animator leftHand;
+    public Animator rightHand;
     public Animator body;
+    public Animator leftLeg;
+    public Animator rightLeg;
     // Use this for initialization
     void Start ()
     {
@@ -21,8 +23,10 @@ public class Runordie : MonoBehaviour {
     void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
-        left.SetBool("Run", !left.GetBool("Run"));
-        right.SetBool("Run", !right.GetBool("Run"));
+        leftHand.SetBool("Run", !leftHand.GetBool("Run"));
+        rightHand.SetBool("Run", !rightHand.GetBool("Run"));
         body.SetBool("Run", !body.GetBool("Run"));
+        leftLeg.SetBool("Run", !leftLeg.GetBool("Run"));
+        rightLeg.SetBool("Run", !rightLeg.GetBool("Run"));
     }
 }
